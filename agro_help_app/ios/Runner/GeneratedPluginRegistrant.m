8735e3419ce83d6f097222f3c0bee16c5d683a41
Marcos Gianetti <marcos.gianetti@carone.com.br>
2021-08-26 23:01:27 -0300

    Inicio

--- agro_help_app/ios/Runner/GeneratedPluginRegistrant.m
+++ agro_help_app/ios/Runner/GeneratedPluginRegistrant.m
@@ -4,9 +4,30 @@
 
 #import "GeneratedPluginRegistrant.h"
 
+#if __has_include(<image_picker/FLTImagePickerPlugin.h>)
+#import <image_picker/FLTImagePickerPlugin.h>
+#else
+@import image_picker;
+#endif
+
+#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
+#import <path_provider/FLTPathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+#if __has_include(<tflite_flutter/TfliteFlutterPlugin.h>)
+#import <tflite_flutter/TfliteFlutterPlugin.h>
+#else
+@import tflite_flutter;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [TfliteFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"TfliteFlutterPlugin"]];
 }
 
 @end
---
+++ agro_help_app/lib/Pages/SubmitImage/submitImage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SubmitImage extends StatelessWidget {
+  const SubmitImage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
