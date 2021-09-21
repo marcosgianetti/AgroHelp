e57dbfdcea3543f4915ce3bdb0d44f73f13eb3de
Marcos Gianetti <marcos.gianetti@carone.com.br>
2021-09-20 23:03:06 -0300

    tela de doenca

--- agro_help_app/lib/Pages/Disease/Details/deseaseDetail.dart
+++ agro_help_app/lib/Pages/Disease/Details/deseaseDetail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import '../../utils.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 Utils _utils = new Utils();
 
@@ -9,83 +10,101 @@ class DeseaseDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // double width = MediaQuery.of(context).size.width;
-    //double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
       appBar: new AppBar(
         title: Center(
-          child: _utils.simpleText('Doença', fontSize: 36),
+          child: _utils.simpleText('Nome Fruta', fontSize: 36),
         ),
         actions: [Image.asset('assets/img/logo.png')],
       ),
-      body: ListWheelScrollView(
-        itemExtent: 90,
-        diameterRatio: 1,
-        children: [
-          Card(
-            child: SizedBox(
-              height: 200,
-              width: 200,
-              child: Center(child: _utils.simpleText('foto', fontSize: 20)),
-            ),
-          ),
-          Card(
-            child: SizedBox(
-              height: 400,
-              width: 400,
-              child: Center(child: _utils.simpleText('foto', fontSize: 20)),
-            ),
-          ),
-          Card(
-            child: SizedBox(
-              height: 400,
-              width: 400,
-              child: Center(child: _utils.simpleText('foto', fontSize: 20)),
-            ),
-          ),
-          Card(
-            child: SizedBox(
-              height: 400,
-              width: 400,
-              child: Center(child: _utils.simpleText('foto', fontSize: 20)),
-            ),
-          ),
-          Card(
-            child: SizedBox(
-              height: 400,
-              width: 400,
-              child: Center(child: _utils.simpleText('foto', fontSize: 20)),
-            ),
-          ),
-        ],
-      ),
-      /* SingleChildScrollView(
-        child: new Center(
-          child: Column(
-            children: [
-              Container(
-                child: _utils.simpleText('Nome da doenca', fontSize: 32),
-              ),
-         
-              Card(
-                child: SizedBox(
-                  height: height / 4,
-                  width: width - 50,
-                  child: Center(child: _utils.simpleText('Carcteristica', fontSize: 20)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: height / 3,
+              child: Container(
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    autoPlay: true,
+                    aspectRatio: 2.0,
+                    enlargeCenterPage: true,
+                  ),
+                  items: [
+                    Card(
+                      child: SizedBox(
+                        height: 400,
+                        width: 400,
+                        child: Center(child: _utils.simpleText('foto', fontSize: 20)),
+                      ),
+                    ),
+                    Card(
+                      child: SizedBox(
+                        height: 400,
+                        width: 400,
+                        child: Center(child: _utils.simpleText('foto', fontSize: 20)),
+                      ),
+                    ),
+                    Card(
+                      child: SizedBox(
+                        height: 400,
+                        width: 400,
+                        child: Center(child: _utils.simpleText('foto', fontSize: 20)),
+                      ),
+                    ),
+                    Card(
+                      child: SizedBox(
+                        height: 400,
+                        width: 400,
+                        child: Center(child: _utils.simpleText('foto', fontSize: 20)),
+                      ),
+                    ),
+                    Card(
+                      child: SizedBox(
+                        height: 400,
+                        width: 400,
+                        child: Center(child: _utils.simpleText('foto', fontSize: 20)),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Card(
-                child: SizedBox(
-                  height: height / 4,
-                  width: width - 50,
-                  child: Center(child: _utils.simpleText('Como tratar', fontSize: 20)),
-                ),
+            ),
+            new Center(
+              child: Column(
+                children: [
+                  Container(
+                    child: _utils.simpleText('Nome da doenca', fontSize: 18),
+                  ),
+                  Card(
+                    child: SizedBox(
+                      height: height / 4,
+                      width: width - 50,
+                      child: Center(child: _utils.simpleText('Carcteristica', fontSize: 20)),
+                    ),
+                  ),
+                  Card(
+                    child: SizedBox(
+                      height: height / 4,
+                      width: width - 50,
+                      child: Center(child: _utils.simpleText('Motivos', fontSize: 20)),
+                    ),
+                  ),
+                  Card(
+                    child: SizedBox(
+                      height: height / 4,
+                      width: width - 50,
+                      child: Center(child: _utils.simpleText('Como tratar', fontSize: 20)),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ),*/
+      ),
     );
   }
 }
